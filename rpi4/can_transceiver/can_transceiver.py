3688e345385f0543cc644f9b08540f092ffb0fa0
Christian Frohn <frohn_c@fev.io>
2024-11-21 18:17:25 +0100

    Implement changes on CAN transceiver to support LED strip implementation

--- rpi4/can_transceiver/can_transceiver.py
+++ rpi4/can_transceiver/can_transceiver.py
@@ -3,6 +3,7 @@ import time
 import can
 from kuksa_client.grpc import VSSClient
 from kuksa_client.grpc import Datapoint
+from threading import Thread
 
 def can_rx(msg: can.Message) -> None:
     # Map value range from 00 to FF to 0 to 100
@@ -11,7 +12,25 @@ def can_rx(msg: can.Message) -> None:
     with VSSClient('127.0.0.1', 55555) as client:
         client.set_current_values({'Vehicle.Cabin.Infotainment.Navigation.Volume': Datapoint(mapped_val)})
 
-def main():
+def kuksa_rx_thread_func():
+    with VSSClient('127.0.0.1', 55555) as client:
+        for updates in client.subscribe_current_values(['Vehicle.Cabin.DoorCount', 'Vehicle.Cabin.SeatRowCount']):
+
+            if 'Vehicle.Cabin.DoorCount' in updates:
+                player_one_score = updates['Vehicle.Cabin.DoorCount'].value
+
+            if 'Vehicle.Cabin.SeatRowCount' in updates:
+                player_two_score = updates['Vehicle.Cabin.SeatRowCount'].value
+
+            with can.Bus(channel="can0", interface="socketcan") as bus:
+                msg = can.Message(arbitration_id=0x13, data=[player_one_score, player_two_score], is_extended_id=False)
+                print(msg)
+                try:
+                    bus.send(msg)
+                except Exception as e:
+                    print("Error sending CAN: " + str(e))
+
+def can_rx_thread_func():
     with can.Bus(channel="can0", interface="socketcan") as bus:
         notifier = can.Notifier(bus, [can_rx])
 
@@ -19,6 +38,14 @@ def main():
             time.sleep(1.0)
 
         notifier.stop()
+
+def main():
+    can_rx_thread = Thread(target=can_rx_thread_func)
+    can_rx_thread.start()
+
+    kuksa_rx_thread = Thread(target=kuksa_rx_thread_func)
+    kuksa_rx_thread.start()
+
 
 if __name__ == "__main__":
     main()
